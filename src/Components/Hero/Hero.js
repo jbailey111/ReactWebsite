6b953c83dfee8c47268969ea64140bcfee985d3f
Jake Bailey <jake.bailey2599@gmail.com>
2023-09-15 00:14:47 +0100

    (3) Added initial hero component

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
 import "./hero.css";
+import heroImage from "../../static/images/hero.jpg";
 
 export default class Hero extends Component {
   render() {
-    return <div className="hero-wrapper">Hero</div>;
+    return (
+      <>
+        <div className="hero-wrapper">
+          <img className="hero-image" src={heroImage} alt="Hero" />
+          <div className="hero-text">
+            <h1 className="hero-title">Hero text</h1>
+            <p className="hero-subtitle">Subtext</p>
+          </div>
+        </div>
+        <div className="hero-wrapper-bounds"></div>
+      </>
+    );
   }
 }
